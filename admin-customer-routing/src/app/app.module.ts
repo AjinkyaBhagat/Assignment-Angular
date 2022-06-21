8bcd8f65a0cf2e1fbcdca0a21e45332db954278a
AjinkyaBhagat <81403374+AjinkyaBhagat@users.noreply.github.com>
2022-06-21 19:25:15 +0530

    adding data-binding assignment

--- admin-customer-routing/src/app/app.module.ts
+++ admin-customer-routing/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 
 
@@ -10,11 +12,13 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
+    DataBindingComponent,
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ admin-customer-routing/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss']
+})
+export class DataBindingComponent implements OnInit {
+  age:any;
+  showAge:any;
+  allData={
+    firstname:"",
+    lastname:"",
+    username:"",
+    email:""
+  }
+  red:string='red';
+  blue:string='blue';
+
+  ageCalculator(){
+    if(this.age){
+      const convertAge = new Date(this.age);
+      const timeDiffernace = Math.abs(Date.now() - convertAge.getTime());
+      this.showAge = Math.floor((timeDiffernace / (1000 * 3600 * 24))/365);
+     
+    }
+  }
+
+  
+  displayEmail="";
+  getEmail(val:string){
+    this.displayEmail=val;
+  }  
+
+  displayUsername="";
+  getUserName(val:string)
+  {
+    this.displayUsername=val;
+  }
+
+ btnClick(){
+  console.log(this.allData)
+}
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
